ed48cc0d8713d4e3273ff3d60c9b496d4108e6b8
higorasilverio <higorasilverio@outlook.com>
2020-07-27 14:24:41 -0300

    Parent component communication with child component

--- comunicacao/src/components/UsuarioInfo.vue
+++ comunicacao/src/components/UsuarioInfo.vue
@@ -2,13 +2,27 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
-        <p>Nome do Usuário: <strong>{{ nome  }}</strong></p>
+        <p>Nome do Usuário: <strong>{{ inverterNome() }}</strong></p>
     </div>
 </template>
 
 <script>
 export default {
-    props:['nome']
+    props: {
+        nome: {
+            type: String,
+            required: true,
+            //default: 'Anônimo'
+            // default: function(){
+            //     return Array(10).fill(0).join(',')
+            // }
+        }
+    },
+    methods: {
+        inverterNome() {
+            return this.nome.split('').reverse().join('')
+        }
+    }
 }
 </script>
 
